20db51cc023f7ffaa14b7da4a8c9eb5e98a5789e
Kent Nguyen <kentingnguyen@yahoo.com>
2013-11-12 22:46:31 -0800

    unable to do anything with tabhost without using deprecated methods

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 PenPal
 ======
+Notes on current implementations
+Chose to use Fragment for the header with Home and Back buttons, since it would be easier to modularize
+Tried to use TabHost to switch between write,draw, and text options, but TabActivity is deprecated.
+	ActionBar Tab does not allow tabs to be at the bottom of the screen
--- src/edu/berkeley/cs160/off_by_1/MakeMessage.java
+++ src/edu/berkeley/cs160/off_by_1/MakeMessage.java
@@ -3,27 +3,36 @@ package edu.berkeley.cs160.off_by_1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.TabActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TabHost;
 
-public class MakeMessage extends Activity {
+public class MakeMessage extends TabActivity {
 
+	String debug = "debug";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_make_message);
 		
-	       TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);  
+		
+	       TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		//TabHost tabHost = getTabHost();
+	       Log.d(debug, "tabhost " + tabHost);
 	       tabHost.addTab(tabHost.newTabSpec("Voice")
-	         .setIndicator("Voice")
-	         .setContent(R.id.voice));
+	         .setIndicator("Voice");
+	         .setContent(R.layout.voice_message);
+	       Log.d(debug, "set voice");
 	       tabHost.addTab(tabHost.newTabSpec("Draw")
 	         .setIndicator("Draw")
 	         .setContent(R.id.draw));
+	       Log.d(debug, "set draw");
 	       tabHost.addTab(tabHost.newTabSpec("Write")
 	         .setIndicator("Write")
 	         .setContent(R.id.write));
+	       Log.d(debug, "set write");
 	      
 	       tabHost.setCurrentTab(0);
 	}
